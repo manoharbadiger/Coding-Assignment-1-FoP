270d82e869a93ead5d1949af7535075eae65d8e9
manoharbadiger <35833750+manoharbadiger@users.noreply.github.com>
2022-06-28 10:46:34 +0530

    assignment-1-FOP

---
+++ HrDepartment.java
@@ -0,0 +1,25 @@
+package com.greatLearning.assignment;
+
+public class HrDepartment extends SuperDepartment {
+
+
+    public String deparmentName() {
+        return " Welcome to HR Department";
+    }
+
+
+    public String getTodaysWork() {
+        return "Fill todays timesheet and mark your attendance";
+    }
+
+
+    public String doActivity() {
+        return "team Lunch";
+    }
+
+
+    public String getWorkDeadline() {
+        return "Complete by EOD ";
+    }
+
+}
---
+++ TechDepartment.java
@@ -0,0 +1,24 @@
+package com.greatLearning.assignment;
+
+public class TechDepartment extends SuperDepartment{
+
+
+	public String deparmentName() {
+		return " Welcome to Tech Department";
+	}
+
+
+	public String getTodaysWork() {
+		return "Complete coding of Module 1";
+	}
+	
+
+	public String getTechStackInformation() {
+		return " Core Java ";
+	}
+
+
+	public String getWorkDeadline() {
+		return "Complete by EOD ";
+	}
+}
